6f1c2297afabb8654f190cc3f05f199970f752c4
bhigoreduardo <bhigoreduardo@gmail.com>
2023-06-10 14:12:07 -0300

    :art: Create auth pages

--- tech-info/store-tech-ecommerce/client/src/components/index.js
+++ tech-info/store-tech-ecommerce/client/src/components/index.js
@@ -1,3 +1,4 @@
 export * as Public from "./Public";
 export * as Common from "./Common";
 export * as Admin from "./Admin";
+export * as Auth from "./Auth";
--- tech-info/store-tech-ecommerce/client/src/layouts/index.js
+++ tech-info/store-tech-ecommerce/client/src/layouts/index.js
@@ -1,2 +1,3 @@
 export { default as Public } from "./Public";
 export { default as Admin } from "./Admin";
+export { default as Auth } from "./Auth";
