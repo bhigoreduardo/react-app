01cde163bd907503b7282da61fd019d89bd533d9
bhigoreduardo <bhigoreduardo@gmail.com>
2023-06-11 21:03:34 -0300

    :wheelchair: Create chat application

---
+++ dawind-app/chat-app/README.md
@@ -0,0 +1,69 @@
+# :desktop_computer: Chat Application
+
+<p align="center"><img src="./assets/cover.png" alt="Cover projeto" /><p>
+<p align="center"><img src="./assets/cover.gif" alt="Cover projeto" /><p>
+
+## :briefcase: Stacks
+
+✅ JavaScript
+✅ Mongo
+✅ Express
+✅ React
+✅ Node
+✅ TailwindCSS
+
+## :hammer: Tools
+
+- Git (`git -v`)
+- NodeJs (`node --version`/`npm --version`)
+- Yarn (`npm install --global yarn`/`yarn --version`)
+- VS Code
+
+## :fire: Run
+
+- Server Development (Port 3001): `yarn dev`
+- Client Development (Port 3000): `yarn dev`
+
+## :baby: Created
+
+- Server: `yarn init -y`
+- Client: `yarn create vite`
+- Tailwind Project: `yarn tailwindcss init -p`
+
+## :ok_man: Dependencies
+
+- Server Dependecies: `yarn add bcryptjs cookie-parser dotenv express jsonwebtoken mongoose ws`
+- Server Dev Dependencies: `yarn add -D nodemon`
+- Client Dependencies: `yarn add axios lodash`
+- Client Dev Dependencies: `yarn add -D autoprefixer postcss tailwind-scrollbar tailwindcss`
+
+## :triangular_flag_on_post: Environment Variables
+
+### Server Variables
+
+- Port: `SERVER_PORT`
+- Mongo: `MONGO_URL`
+- Secret: `JWT_SECRET`
+- Salt `BCRYPT_SALT`
+- Client Address: `CLIENT_URL`
+
+### Client Variables
+
+- Server Address: `VITE_SERVER_URL`
+- Socket: `VITE_WEBSOCKET_URL`
+
+## :page_facing_up: Docs
+
+<details>
+<summary><b>Documentações</b></summary>
+
+### :scroll: Documentações
+
+- Axios: `https://axios-http.com/`
+- React Icons: `https://react-icons.github.io/react-icons/`
+- Router Dom: `https://reactrouter.com/en/main`
+- Vite: `https://vitejs.dev/`
+- Tailwindcss: `https://tailwindcss.com/`
+- HeroIcons: `https://heroicons.com/`
+
+<details>
---
+++ dawind-app/chat-app/client/src/components/Chat.jsx
@@ -0,0 +1,270 @@
+import { useContext, useState, useEffect, useRef } from "react";
+import axios from "axios";
+import { uniqBy } from "lodash";
+
+import { UserContext } from "../context/UserContext";
+
+import Logo from "./Logo";
+import Contact from "./Contact";
+
+export default function Chat() {
+  const [ws, setWs] = useState(null);
+  const [selectedUserId, setSelectedUserId] = useState(null);
+  const [onlinePeople, setOnlinePeople] = useState({});
+  const [offlinePeople, setOffilePeople] = useState({});
+  const [newMessage, setNewMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const divUnderMessage = useRef();
+
+  const { id, setId, username, setUsername } = useContext(UserContext);
+
+  function getWsConnect() {
+    const ws = new WebSocket(import.meta.env.VITE_WEBSOCKET_URL); // 'ws://localhost:3001'
+    setWs(ws);
+    ws.addEventListener("message", handleMessage);
+    ws.addEventListener("close", () => {
+      setTimeout(() => {
+        getWsConnect();
+      }, 1000);
+    });
+  }
+  function handleMessage(ev) {
+    const messageData = JSON.parse(ev.data);
+
+    if ("online" in messageData) {
+      showOnlinePeople(messageData.online);
+    } else if ("text" in messageData) {
+      // setMessages(prevState => ([...prevState, { text: messageData.text, isOur: false }]))
+      // if (messageData.sender === selectedUserId && recipient === id)
+      // if (messageData.sender === selectedUserId)
+      setMessages((prevState) => [...prevState, { ...messageData }]);
+    }
+  }
+  function showOnlinePeople(messageData) {
+    const people = {};
+    messageData.forEach(({ userId, username }) => {
+      people[userId] = username;
+    });
+    setOnlinePeople(people);
+  }
+
+  function sendMessage(ev, file = null) {
+    if (ev) ev.preventDefault();
+
+    ws.send(
+      JSON.stringify({
+        recipient: selectedUserId,
+        text: newMessage,
+        file,
+      })
+    );
+
+    setNewMessage("");
+    // setMessages(prevState => ([...prevState, { text: newMessage, isOur: true }]))
+    if (!file) {
+      setMessages((prevState) => [
+        ...prevState,
+        {
+          text: newMessage,
+          sender: id,
+          recipient: selectedUserId,
+          _id: Date.now(),
+        },
+      ]);
+    } else {
+      axios.get(`/messages/${selectedUserId}`).then((res) => {
+        setMessages(res.data);
+      });
+    }
+  }
+  function sendFile(ev) {
+    const reader = new FileReader();
+    reader.readAsDataURL(ev.target.files[0]);
+    reader.onload = () => {
+      sendMessage(null, {
+        name: ev.target.files[0].name,
+        data: reader.result,
+      });
+    };
+  }
+
+  function loggout() {
+    axios.post("/loggout").then((res) => {
+      console.log(res.data);
+      setWs(null);
+      setId(null);
+      setUsername(null);
+    });
+  }
+
+  useEffect(() => {
+    getWsConnect();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    axios.get("/people").then((res) => {
+      const offile = res.data
+        .filter((item) => item._id !== id)
+        .filter((item) => !Object.keys(onlinePeople).includes(item._id));
+      const offlinePeople = {};
+      offile.forEach((item) => {
+        offlinePeople[item._id] = item.username;
+      });
+      setOffilePeople(offlinePeople);
+    });
+  }, [onlinePeople]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    if (selectedUserId) {
+      axios.get(`/messages/${selectedUserId}`).then((res) => {
+        setMessages(res.data);
+      });
+    }
+  }, [selectedUserId]);
+
+  useEffect(() => {
+    const div = divUnderMessage.current;
+    if (div) div.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+  const onlinePeopleExcludeLogged = { ...onlinePeople };
+  delete onlinePeopleExcludeLogged[id];
+  const messageWithoutDupes = uniqBy(messages, "_id");
+
+  return (
+    <section className="flex h-screen">
+      <div className="w-1/3 flex flex-col bg-white">
+        <div className="flex-grow">
+          <Logo />
+          {Object.keys(onlinePeopleExcludeLogged).map((item) => (
+            <Contact
+              key={item}
+              userId={item}
+              username={onlinePeopleExcludeLogged[item]}
+              selectedUserId={selectedUserId}
+              setSelectedUserId={setSelectedUserId}
+              isOnline={true}
+            />
+          ))}
+          {Object.keys(offlinePeople).map((item) => (
+            <Contact
+              key={item}
+              userId={item}
+              username={offlinePeople[item]}
+              selectedUserId={selectedUserId}
+              setSelectedUserId={setSelectedUserId}
+              isOnline={false}
+            />
+          ))}
+        </div>
+
+        <div className="flex flex-col items-center justify-center pb-2 gap-2">
+          <p className="flex items-center gap-2 justify-center text-sm text-gray-600">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              className="w-6 h-6 fill-current"
+            >
+              <path d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" />
+            </svg>
+
+            {username}
+          </p>
+
+          <button
+            onClick={loggout}
+            className="px-3 py-2 bg-blue-400 text-sm text-gray-800 border rounded-sm"
+          >
+            Loggout
+          </button>
+        </div>
+      </div>
+
+      <div className="w-2/3 flex flex-col p-2 bg-blue-50">
+        <div className="flex-grow">
+          {!selectedUserId && (
+            <div className="flex items-center justify-center h-full text-gray-500">
+              &larr; Select a person from the sidebar
+            </div>
+          )}
+          {!!selectedUserId && (
+            <div className="h-full relative">
+              <div className="absolute top-0 left-0 right-0 bottom-2 overflow-y-auto scrollbar-none">
+                {messageWithoutDupes.map((item) => (
+                  <div
+                    key={item._id}
+                    className={`${
+                      item.sender === id ? "text-right" : "text-left"
+                    }`}
+                  >
+                    <div
+                      className={`inline-block p-2 my-2 text-sm rounded-md ${
+                        item.sender === id
+                          ? "bg-blue-500 text-white"
+                          : "bg-white text-gray-500"
+                      }`}
+                    >
+                      {item.text}
+                      {item.file && (
+                        // eslint-disable-next-line react/jsx-no-target-blank
+                        <a
+                          href={`${axios.defaults.baseURL}/src/assets/uploads/${item.file}`}
+                          target="_blank"
+                          className="flex items-center gap-1 border-b"
+                        >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            className="w-4 h-4 fill-current"
+                          >
+                            <path d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z" />
+                          </svg>
+                          {item.file}
+                        </a>
+                      )}
+                    </div>
+                  </div>
+                ))}
+                <div ref={divUnderMessage}></div>
+              </div>
+            </div>
+          )}
+        </div>
+        {!!selectedUserId && (
+          <form className="flex gap-2" onSubmit={sendMessage}>
+            <input
+              type="text"
+              placeholder="Type your message"
+              value={newMessage}
+              onChange={(ev) => setNewMessage(ev.target.value)}
+              className="bg-white p-2 border rounded-sm flex-grow"
+            />
+
+            <label className="bg-gray-200 p-2 text-gray-500 border rounded-sm cursor-pointer">
+              <input type="file" className="hidden" onChange={sendFile} />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                className="w-6 h-6 fill-current"
+              >
+                <path d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z" />
+              </svg>
+            </label>
+
+            <button
+              type="submit"
+              className="bg-blue-500 p-2 text-white border rounded-sm"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6 stroke-2 stroke-white fill-none"
+              >
+                <path d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+              </svg>
+            </button>
+          </form>
+        )}
+      </div>
+    </section>
+  );
+}
