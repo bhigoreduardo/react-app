f733be8ad47a184049fbfa78879988c406ba197e
bhigoreduardo <bhigoreduardo@gmail.com>
2023-06-19 16:13:57 -0300

    :art: Create product page

--- dev-corner-app/store-tech-app/client/src/App.jsx
+++ dev-corner-app/store-tech-app/client/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Store from "./pages/Store";
+import Product from "./pages/Product";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/loja" element={<Store />} />
+          <Route path="/produto" element={<Product />} />
         </Route>
       </Routes>
     </Router>
